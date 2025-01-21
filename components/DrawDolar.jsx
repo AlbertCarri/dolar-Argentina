6e6230cfade0d6ccb500e8256a93c79dfe467eac
Alberto Carrizo <ae_carrizo@hotmail.com>
2025-01-20 23:00:25 -0300

    Responsive fail repaired

--- components/DrawDolar.jsx
+++ components/DrawDolar.jsx
@@ -1,7 +1,7 @@
 export default function DrawDolar({ dolar }) {
   const color=['bg-green-800','bg-blue-900','bg-gray-700','bg-amber-900','bg-teal-900','bg-yellow-600','bg-red-800']
   return (
-    <div className="grid 2xl:grid-cols-3 xl:grid-cols-2 grid-cols-1 w-1/2 m-auto rounded-2xl py-4 gap-1">
+    <div className="grid 2xl:grid-cols-3 xl:grid-cols-2 grid-cols-1 lg:w-1/2 w-4/5 m-auto rounded-2xl py-4 gap-2">
       {dolar.map((dolares, index) => (
         <div key={index} className={`${color[index]} text-stone-200 flex flex-col w-60 m-auto mt-4 mb-2 p-4 rounded-xl items-start shadow-black shadow-lg`}>
           <div className="text-xs mb-2 uppercase font-bold italic m-auto ">Dolar {dolares.nombre}</div>
